647bca03e2bd9698d65262a7b4129aadafce275b
EliteDev9472 <ben.alexander9072@gmail.com>
2024-11-26 14:32:47 -0500

    upate

--- utils/data/personal-data.js
+++ utils/data/personal-data.js
@@ -1,8 +1,14 @@
 export const personalData = {
-  name: "Elite Dev",
+  name: "EliteDev",
   profile: '/profile.png',
   designation: "Software Developer",
-  description: 'I am EliteDev. Full Stack Developer with extensive experience in Java, JavaScript,Express, Node.js, Nest.js, Nuxt ,Next, Angular, Vue, React, Python,FastAPI, Django, Flask, Laravel. Participate development of $500K research project which was deemed a "gold standard" by the client. Increased client`s revenue 2-fold after fine-tuning AI/ML-based algorithms. Well-acquainted with HR methodologies. Highly skilled and results-oriented AI Engineer with a track record of developing and implementing cutting-edge AI algorithms and models to solve complex problems.',
+  description: "Full Stack Developer with extensive experience in Java, JavaScript,Express, Node.js, Nest.js, Nuxt ,Next, Angular, Vue, React, Python,FastAPI, Django, Flask, Laravel. Participate development of $500K research project which was " +
+    "deemed a 'gold standard' by the client. Increased client's revenue 2-fold after finetuning AI/ML-based algorithms. Well-acquainted with HR methodologies. Highly " +
+    "skilled and results-oriented AI Engineer with a track record of developing and " + +
+    "implementing cutting-edge AI algorithms and models to solve complex problems. " +
+    "Innovative Blockchain Developer with extensive experience leveraging Modern " +
+    "Blockchain Technology to design and deploy cutting-edge decentralized " +
+    "applications.",
   email: 'elitebizpro9472@gmail.com',
   phone: '+1 316 207 6321',
   address: '1523 greenlake drive Aurora IL, US - 60502',
