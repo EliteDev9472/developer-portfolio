d6630e34c745ee246923a66092aa3bdf4ffe2937
EliteDev9472 <ben.alexander9072@gmail.com>
2024-11-26 14:27:19 -0500

    update

--- app/page.js
+++ app/page.js
@@ -1,4 +1,5 @@
 import { personalData } from "@/utils/data/personal-data";
+import dynamic from 'next/dynamic';
 import AboutSection from "./components/homepage/about";
 import Blog from "./components/homepage/blog";
 import ContactSection from "./components/homepage/contact";
